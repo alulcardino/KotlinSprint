fc12f70ceaddcabc0b4dd2e0179dd2b278522637
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-27 21:52:53 +0600

    ks-1-4 is done

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,4 +1,21 @@
 package lesson_1
 
 class lesson1_task4 {
+
+    fun main() {
+        val length: Long = 40868600000
+        val age: Byte = 27
+        val partOfDay: Float = 0.075f
+        val seconds: Short = 6480
+        val partOfYear: Double = 2.0547945205479453E-4
+        val apogee: Int = 327000
+
+        println(length)
+        println(age)
+        println(partOfDay)
+        println(seconds)
+        println(partOfYear)
+        println(apogee)
+    }
+
 }
