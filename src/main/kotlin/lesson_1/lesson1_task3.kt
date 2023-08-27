c999087b2e06a932c4a50578cdb5f622bec0ad0e
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-27 21:37:21 +0600

    ks-1-3 is done

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,4 +1,21 @@
 package lesson_1
 
 class lesson1_task3 {
+
+    fun main() {
+        val year = 1961
+        var hour = "09"
+        var minute = "07"
+
+        println(year)
+        println(hour)
+        println(minute)
+
+        hour = "10"
+        minute = "55"
+
+        println("$hour:$minute")
+
+    }
+
 }
