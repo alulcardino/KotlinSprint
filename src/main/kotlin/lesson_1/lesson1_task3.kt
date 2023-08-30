94bf49390627caa555c00d5e0ff15d8f6477ee3f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-30 17:01:01 +0600

    KS-1-3 info about space is added

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,21 +1,18 @@
 package lesson_1
 
-class lesson1_task3 {
 
-    fun main() {
-        val year = 1961
-        var hour = "09"
-        var minute = "07"
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
 
-        println(year)
-        println(hour)
-        println(minute)
+    println(year)
+    println(hour)
+    println(minute)
 
-        hour = "10"
-        minute = "55"
+    hour = "10"
+    minute = "55"
 
-        println("$hour:$minute")
-
-    }
+    println("$hour:$minute")
 
 }
