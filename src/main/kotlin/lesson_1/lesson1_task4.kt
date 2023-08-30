7a1cf7e695ca14583f145b6e10efb647103bbef0
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-30 17:50:54 +0600

    KS-1-5 amount oftime inspace is added

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,21 +1,4 @@
 package lesson_1
 
 class lesson1_task4 {
-
-    fun main() {
-        val seconds = 6480
-        val minutes = seconds / 60
-        val remainSeconds = seconds % 60
-        val hours = minutes / 60
-        val remainMinutes = minutes % 60
-
-        println(seconds)
-        println(minutes)
-        println(remainSeconds)
-        println(hours)
-        println(remainMinutes)
-
-        println("0$hours:$remainMinutes:0$remainSeconds")
-
-    }
 }
