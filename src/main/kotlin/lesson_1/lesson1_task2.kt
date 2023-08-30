30692b87614dee452fa0bc3bf8b6d0dc377b28de
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-30 16:52:28 +0600

    KS-1-2 count of workers is added

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,4 +1,17 @@
 package lesson_1
 
-class lesson1_task2 {
+fun main() {
+    val countOfOrder: Int = 75
+    val thanking: String = "Thank you for purchase"
+
+    println(countOfOrder)
+    println(thanking)
+
+    var countOfWorkers = 2000
+
+   // println(countOfWorkers)
+
+    countOfWorkers--
+
+    println(countOfWorkers)
 }
