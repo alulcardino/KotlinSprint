bfefa005cdbb100ec80fdb4655d69ba537104df6
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-31 21:31:50 +0600

    KS-2-5 info about buff is added

--- src/main/kotlin/lesson2_task1/lesson2_task5.kt
+++ src/main/kotlin/lesson2_task1/lesson2_task5.kt
@@ -1,2 +1,14 @@
 package lesson2_task1
 
+import kotlin.math.pow
+
+fun main() {
+    val startDeposit = 70_000
+    val percentOfDeposit = 16.7
+    val amountOfYears = 20
+
+    val finalDeposit =  startDeposit * (1 + (percentOfDeposit / 100)).pow(amountOfYears)
+
+    println(String.format("%.3f", finalDeposit))
+
+}
