f00f03d62762473dcea38d7351ad84fcb63b3257
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-08-31 21:14:31 +0600

    KS-2-4 info about buff is added

--- src/main/kotlin/lesson2_task1/lesson2_task4.kt
+++ src/main/kotlin/lesson2_task1/lesson2_task4.kt
@@ -1,2 +1,14 @@
 package lesson2_task1
 
+fun main () {
+
+    val crystalOre = 7
+    val ironOre = 11
+    val percentOfBuff = 20
+
+    val buffCrystalOre = (crystalOre / 100.0) * percentOfBuff
+    val buffIronOre  = (ironOre / 100.0) * percentOfBuff
+
+    println(buffCrystalOre.toInt())
+    println(buffIronOre.toInt())
+}
