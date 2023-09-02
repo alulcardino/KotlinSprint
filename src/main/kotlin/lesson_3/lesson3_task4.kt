74b73bdbe31772680116c3e94cb6af0813a4722c
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-02 14:07:22 +0600

    KS-3-4 send first move chess is added

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,5 +1,16 @@
 package lesson_3
 
 fun main() {
+    var from = "E2"
+    var to = "E4"
+    var numberOfMove = 1
+
+    println("$from-$to;$numberOfMove")
+
+    from = "D2"
+    to = "D3"
+    numberOfMove++
+
+    println("$from-$to;$numberOfMove")
 
 }
