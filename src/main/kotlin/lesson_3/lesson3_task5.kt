df6541b64c4888841b6405bd698b874341dc712e
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-02 14:13:27 +0600

    KS-3-5 get first move chess is added

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,5 +1,13 @@
 package lesson_3
 
 fun main() {
+    val move = "D2-D4;0"
 
+    val from = move.substring(0,2)
+    val to = move.substring(3,5)
+    val numberOfMove = move.substring(6,7)
+
+    println(from)
+    println(to)
+    println(numberOfMove)
 }
