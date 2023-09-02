2ed485a5c27cdf44af7b3db4dfa2086c768bb067
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-02 13:43:16 +0600

    KS-3-1 greetings for user is added

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,4 +2,13 @@ package lesson_3
 
 fun main() {
 
+    val name = "Name"
+    var greetings = "Good morning"
+
+    println("$greetings, $name")
+
+    greetings = "Good evening"
+
+    println("$greetings, $name")
+
 }
