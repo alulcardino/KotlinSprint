781f896bc048571a7a161fd738b224c6fdab34b9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-03 21:28:57 +0600

    KS-4-3 favorable conditions is added

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,5 +1,10 @@
 package lesson_4
 
 fun main() {
+    val isSunny: Boolean = true
+    val tentIsOpen: Boolean = true
+    val humidity: Int = 20
+    val season: String = "winter"
 
+    println(isSunny && tentIsOpen && humidity == 20 && season != "winter")
 }
