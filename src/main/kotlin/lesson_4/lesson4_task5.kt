6860891e9b8f46f4fee98ff3bd959bcece34b071
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-03 21:06:27 +0600

    KS-4-1 amount of free places is added

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,10 +1,5 @@
 package lesson_4
 
 fun main() {
-    val amountOfPlaces = 13
-    val amountOfFreePlacesToday = 0
-    val amountOfFreePlacesTomorrow = 4
 
-    println("Доступность столиков на сегодня: ${amountOfPlaces - amountOfFreePlacesToday != 13}")
-    println("Доступность столиков на завтра: ${amountOfPlaces - amountOfFreePlacesTomorrow != 13}")
 }
