ca71b1efbdbf23ba79f7eb5ac909bd7bc83e7680
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-03 21:17:06 +0600

    KS-4-2 customs terminal category is added

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,5 +1,15 @@
 package lesson_4
 
 fun main() {
+    val category = "Average"
+    var weight = 20
+    var volume = 80
+
+    println(weight >= 35 && weight <= 100 && volume < 100)
+
+    weight = 50
+    volume = 100
+
+    println(weight >= 35 && weight <= 100 && volume < 100)
 
 }
