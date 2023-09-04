8add3f91af3227cedb79319612ba8afec4d8e66f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-04 22:36:34 +0600

    KS-4-3 maked const

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,10 +1,20 @@
 package lesson_4
 
+const val IS_SUNNY = true
+const val TENT_IS_OPEN = true
+const val HUMIDITY = 20
+const val SEASON = "not winter"
+
 fun main() {
     val isSunny: Boolean = true
     val tentIsOpen: Boolean = true
     val humidity: Int = 20
     val season: String = "winter"
 
-    println(isSunny && tentIsOpen && humidity == 20 && season != "winter")
+    println(
+        isSunny == IS_SUNNY
+                && tentIsOpen == TENT_IS_OPEN
+                && humidity == HUMIDITY
+                && season == SEASON
+    )
 }
