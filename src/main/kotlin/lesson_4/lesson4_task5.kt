94363b716bba47cd9d6df7192471e7ddd6f99920
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-05 00:19:30 +0600

    KS-5-1 deleted from wrong class

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,18 +1,5 @@
 package lesson_4
 
 fun main() {
-    val firstValue = 3;
-    val secondValue = 2;
-    val trueAnswer = firstValue + secondValue
 
-
-    println("5You need to solve to prove what you are human: $firstValue + $secondValue = ")
-
-    val answer = readLine()?.toInt()
-
-    if (answer == trueAnswer) {
-        println("Welcome!")
-    } else {
-        println("Access Denied!")
-    }
 }
