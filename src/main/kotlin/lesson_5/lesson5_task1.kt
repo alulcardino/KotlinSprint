24bb7093341dbd6c132b0478cd3a2843a9ec6dfc
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-05 00:21:16 +0600

    KS-5-1 moved to right class

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,18 @@
+package lesson_5
+
+fun main() {
+    val firstValue = 3;
+    val secondValue = 2;
+    val trueAnswer = firstValue + secondValue
+
+
+    println("5You need to solve to prove what you are human: $firstValue + $secondValue = ")
+
+    val answer = readLine()?.toInt()
+
+    if (answer == trueAnswer) {
+        println("Welcome!")
+    } else {
+        println("Access Denied!")
+    }
+}
