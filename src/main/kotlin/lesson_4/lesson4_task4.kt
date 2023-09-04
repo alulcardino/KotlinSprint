07b5fc9c10e13084309ba1b707b8af38f845e62c
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-04 22:52:26 +0600

    KS-4-4 added easy removing and deleting types of train

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,15 +3,17 @@ package lesson_4
 fun main() {
     val day = 5;
 
-    val isHandAndPressDay = day % 2 == 1
-    val isLegAndBackDya = !isHandAndPressDay
+    val isHandDay = day % 2 == 1
+    val isPressDay = day % 2 == 1
+    val isLegDay = day % 2 == 0
+    val isBackDay = day % 2 == 0
 
     println(
         """
-        Упражнения для рук:   $isHandAndPressDay
-        Упражнения для ног:   $isLegAndBackDya    
-        Упражнения для спины: $isHandAndPressDay
-        Упражнения для пресса:$isLegAndBackDya    
+        Упражнения для рук:   $isHandDay
+        Упражнения для ног:   $isLegDay    
+        Упражнения для спины: $isPressDay
+        Упражнения для пресса:$isBackDay    
     """.trimIndent()
     )
 
