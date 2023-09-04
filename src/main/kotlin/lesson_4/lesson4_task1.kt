8b6dc96137a663b6a529749bc5131ae7a29cbbf4
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-04 22:28:35 +0600

    KS-4-1 maked amount of places const and rewrited conditions

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,9 +1,14 @@
 package lesson_4
 
+const val AMOUNT_OF_PLACES = 13
+
 fun main() {
-    val amountOfPlaces = 13
     val amountOfFreePlacesToday = 0
     val amountOfFreePlacesTomorrow = 4
-    println ("Доступность столиков на сегодня: ${amountOfPlaces - amountOfFreePlacesToday != 13}")
-    println ("Доступность столиков на завтра: ${amountOfPlaces - amountOfFreePlacesTomorrow != 13}")
+
+    val  amountOfTakenPlacesToday = AMOUNT_OF_PLACES - amountOfFreePlacesToday
+    val  amountOfTakenPlacesTomorrow = AMOUNT_OF_PLACES - amountOfFreePlacesTomorrow
+
+    println ("Доступность столиков на сегодня: ${AMOUNT_OF_PLACES - amountOfTakenPlacesToday != 0}")
+    println ("Доступность столиков на завтра: ${AMOUNT_OF_PLACES - amountOfTakenPlacesTomorrow != 0}")
 }
