6f43eded39c127c7123532dab3325aa4b4e7a632
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-05 00:43:03 +0600

    KS-5-3 lottery is added

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,5 +1,20 @@
 package lesson_5
 
 fun main() {
+    val firstNumber = 5;
+    val secondNumber = 78;
 
+    print("Enter first number: ")
+    val userFirstNumber = readLine()!!.toInt()
+
+    print("Enter second number: ")
+    val userSecondNumber = readLine()!!.toInt()
+
+    if (userFirstNumber == firstNumber && userSecondNumber == secondNumber) {
+        println("You win main prize")
+    } else if (userFirstNumber == firstNumber || userSecondNumber == secondNumber) {
+        println("You win second prize")
+    } else {
+        println("You lose")
+    }
 }
