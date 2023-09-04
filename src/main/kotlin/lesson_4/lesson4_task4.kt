c7ff2060f2a25482f0025d019230a936923ac929
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-04 22:49:18 +0600

    KS-4-4 code move to right file

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,5 +1,18 @@
 package lesson_4
 
 fun main() {
+    val day = 5;
+
+    val isHandAndPressDay = day % 2 == 1
+    val isLegAndBackDya = !isHandAndPressDay
+
+    println(
+        """
+        Упражнения для рук:   $isHandAndPressDay
+        Упражнения для ног:   $isLegAndBackDya    
+        Упражнения для спины: $isHandAndPressDay
+        Упражнения для пресса:$isLegAndBackDya    
+    """.trimIndent()
+    )
 
 }
