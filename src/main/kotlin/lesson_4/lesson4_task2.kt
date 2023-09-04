9da0c714c5a62ded5a644a474a2625a06a6873d0
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-04 22:32:00 +0600

    KS-4-1 maked const

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,15 +1,18 @@
 package lesson_4
 
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_MAX = 100
+
 fun main() {
-    val category = "Average"
     var weight = 20
     var volume = 80
 
-    println(weight >= 35 && weight <= 100 && volume < 100)
+    println(weight >= WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX)
 
     weight = 50
     volume = 100
 
-    println(weight >= 35 && weight <= 100 && volume < 100)
+    println(weight >= WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX)
 
 }
