22353d9ceb42c8e3643be27a1ef26f24ecf8a684
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-05 00:35:41 +0600

    KS-5-2 adult age is added

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,5 +1,13 @@
 package lesson_5
 
-fun main() {
+const val ADULT_AGE = 18
 
+fun main() {
+    val currentYear = 2023
+    print("Enter your birth year: ")
+    val birthYear = readLine()!!.toInt()
+    val age = currentYear - birthYear
+    if (age >= ADULT_AGE) {
+        println("Show adult content")
+    }
 }
