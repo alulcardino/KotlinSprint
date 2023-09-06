718c168c6a7354c242374bff547b935580d409c7
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-06 17:33:55 +0600

    KS-5-5 lottery with random is added

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,5 +1,30 @@
 package lesson_5
 
+import kotlin.random.Random
+
+const val MIN = 1;
+const val MAX = 100;
+
 fun main() {
+
+    val firstNumber = (MIN..MAX).random();
+    val secondNumber = (MIN..MAX).random();
+
+    print("Enter first number: ")
+    val userFirstNumber = readLine()!!.toInt()
+
+    print("Enter second number: ")
+    val userSecondNumber = readLine()!!.toInt()
+
+    if (userFirstNumber == firstNumber && userSecondNumber == secondNumber) {
+        println("You win main prize")
+    } else if (userFirstNumber == firstNumber || userSecondNumber == secondNumber) {
+        println("You win second prize")
+    } else {
+        println("You lose")
+    }
+
+    println("First number: $firstNumber")
+    println("Second number: $secondNumber")
 
 }
