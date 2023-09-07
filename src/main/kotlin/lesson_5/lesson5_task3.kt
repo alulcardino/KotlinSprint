864a68b71205e9a89864d191eb905337b9cbbcda
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-07 21:27:12 +0600

    KS-5-3 refactored readline

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -5,12 +5,13 @@ fun main() {
     val secondNumber = 78;
 
     print("Enter first number: ")
-    val userFirstNumber = readLine()!!.toInt()
+    val userFirstNumber = readLine()?.toInt() ?: 0
 
     print("Enter second number: ")
-    val userSecondNumber = readLine()!!.toInt()
+    val userSecondNumber = readLine()?.toInt() ?: 0
 
-    if (userFirstNumber == firstNumber && userSecondNumber == secondNumber) {
+    if (userFirstNumber == firstNumber && userSecondNumber == secondNumber ||
+            userSecondNumber == firstNumber && userFirstNumber == secondNumber) {
         println("You win main prize")
     } else if (userFirstNumber == firstNumber || userSecondNumber == secondNumber) {
         println("You win second prize")
