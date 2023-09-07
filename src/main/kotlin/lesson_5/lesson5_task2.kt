5a8f10f8f22a2543d28d01b9cd57cd5dfb9d656a
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-07 21:06:44 +0600

    KS-5-2 refactored readline

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -5,7 +5,7 @@ const val ADULT_AGE = 18
 fun main() {
     val currentYear = 2023
     print("Enter your birth year: ")
-    val birthYear = readLine()!!.toInt()
+    val birthYear = readLine()?.toInt() ?: 0
     val age = currentYear - birthYear
     if (age >= ADULT_AGE) {
         println("Show adult content")
