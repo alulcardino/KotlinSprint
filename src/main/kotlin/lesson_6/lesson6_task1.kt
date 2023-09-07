2226050329f8f3b790cbce439ed4d7fa2f871770
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-07 21:46:55 +0600

    KS-6-1 register check is added

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -1,5 +1,22 @@
 package lesson_6
 
 fun main() {
+    print("Create login: ")
+    val registerLogin = readln()
+    print("Create password: ")
+    val registerPassword = readln()
 
+    print("Enter login: ")
+    var login: String = readln()
+    print("Enter password: ")
+    var password: String = readln()
+
+    while (login != registerLogin || password != registerPassword) {
+        println("Wrong password or login. Please, enter again")
+        print("Enter login: ")
+        login = readln()
+        print("Enter password: ")
+        password = readln()
+    }
+    println("You successfully entered")
 }
