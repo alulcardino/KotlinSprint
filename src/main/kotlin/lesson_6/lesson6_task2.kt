7a6ca73bb1f0f48738c950056d0d8978cbfc7527
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-08 22:47:29 +0600

    KS-6-2 timer is added

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,5 +1,15 @@
 package lesson_6
 
 fun main() {
+    var counter = 0;
 
+    print("Введите кол-во секунд: ")
+    val amountOfSeconds = readLine()?.toInt() ?: 0
+
+    while (counter <= amountOfSeconds) {
+        Thread.sleep(1000)
+        counter++
+    }
+
+    println("Прошло $amountOfSeconds секунд")
 }
