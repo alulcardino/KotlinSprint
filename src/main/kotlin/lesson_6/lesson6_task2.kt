4a073b8fc26edb06799d5bd7f9e07d08b3472bc5
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-08 22:55:50 +0600

    KS-6-3 moved to right file

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,17 +1,5 @@
 package lesson_6
 
 fun main() {
-    var counter = 0;
-
-    print("Введите кол-во секунд: ")
-    val amountOfSeconds = readLine()?.toInt() ?: 0
-
-    while (counter < amountOfSeconds) {
-        println("Осталось секунд: ${amountOfSeconds - counter}")
-        Thread.sleep(1000)
-        counter++
-    }
-
-    println("Время вышло")
 
 }
