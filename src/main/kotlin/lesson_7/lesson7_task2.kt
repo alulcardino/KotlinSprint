cf458e328bb8989d8f13e784702cd47703687cf9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-10 01:47:36 +0600

    KS-7-2 sms check is added

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,5 +1,23 @@
 package lesson_7
 
+const val MIN = 1000
+const val MAX = 9999
+
 fun main() {
+    var smsNumber: Int
+    var userCode: Int
+
+    while (true) {
+        smsNumber = (MIN..MAX).random()
+        println("""Ваш код авторизации: $smsNumber
+            |Введите код: 
+        """.trimMargin())
+        userCode = readLine()?.toInt() ?: 0
+        if (userCode == smsNumber) {
+            break
+        }
+    }
+
+    println("Добро пожаловать!")
 
 }
