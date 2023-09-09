28b12b5383cee6f96dd5916bbad0726b1691ffae
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-10 01:19:40 +0600

    KS-6-5 math captcha is added

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,5 +1,32 @@
 package lesson_6
 
+const val MIN = 1
+const val MAX = 9
+const val MAX_ATTEMPTS = 1
+
 fun main() {
+    var firstNumber: Int
+    var secondNumber: Int
+    var answer: Int
+    var rightAnswer: Int
+    var attempt = 0;
+
+    while (attempt < MAX_ATTEMPTS) {
+        firstNumber = (MIN..MAX).random()
+        secondNumber = (MIN..MAX).random()
+        rightAnswer = secondNumber + firstNumber
+
+        print("$firstNumber + $secondNumber = ")
+        answer = readLine()?.toInt() ?: 0
+
+        if (answer == rightAnswer) {
+            println("Welcome!")
+            break
+        }
+
+        attempt++
+    }
+
+
 
 }
