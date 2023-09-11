e67cae44e2588396c8819a97180f92f6a9a4e8a5
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-11 21:51:10 +0600

    KS-7-5 hard password is added

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,5 +1,18 @@
 package lesson_7
 
-fun main() {
+import kotlin.random.Random
 
+const val CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+
+fun main() {
+    print("Enter length of password: ")
+    val length = readLine()?.toInt() ?: 0
+    val password = StringBuilder("")
+
+    for (i in 1..length) {
+        password.append(CHARACTERS[Random.nextInt(CHARACTERS.length)])
+    }
+
+    println("Your password is $password")
 }
