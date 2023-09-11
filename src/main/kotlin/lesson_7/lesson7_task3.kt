2ca6eb4e6f3c65171cd7a482217b7eda813cfaef
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-11 21:29:50 +0600

    KS-7-4 timer is added

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -1,5 +1,15 @@
 package lesson_7
 
-fun main() {
+const val MILISECONDS_IN_SECOND = 1000L
 
+fun main() {
+    print("Введите кол-во секунд: ")
+    val amountOfSeconds = readLine()?.toInt() ?: 0
+
+    for (i in 1..amountOfSeconds) {
+        Thread.sleep(i * MILISECONDS_IN_SECOND)
+        println("Прошло: $i секунд")
+    }
+
+    println("Время вышло")
 }
