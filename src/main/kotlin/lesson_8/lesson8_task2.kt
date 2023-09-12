ce1f3dfad4f2115db47cb0a2fed8ea97d8f27114
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-12 16:42:09 +0600

    KS-8-3 moved to right file

--- src/main/kotlin/lesson_8/lesson8_task2.kt
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -1,14 +1,5 @@
 package lesson_8
 
 fun main() {
-    val recipe = arrayListOf("Egg", "Butter", "Apple", "Banana", "Milk", "Sugar")
 
-    print("Enter ingredient: ")
-    val userIngredient = readLine().toString()
-
-    if (recipe.contains(userIngredient)) {
-        print("The recipe has $userIngredient")
-    } else {
-        println("The recipe does not have $userIngredient")
-    }
 }
