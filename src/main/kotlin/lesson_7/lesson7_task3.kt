4443d10373023946c69d9b0586fd08e20d57bc3f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-12 15:38:22 +0600

    KS-7-4 moved to right file

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -1,15 +1,5 @@
 package lesson_7
 
-const val MILISECONDS_IN_SECOND = 1000L
-
 fun main() {
-    print("Введите кол-во секунд: ")
-    val amountOfSeconds = readLine()?.toInt() ?: 0
 
-    for (i in 1..amountOfSeconds) {
-        Thread.sleep(i * MILISECONDS_IN_SECOND)
-        println("Прошло: $i секунд")
-    }
-
-    println("Время вышло")
 }
