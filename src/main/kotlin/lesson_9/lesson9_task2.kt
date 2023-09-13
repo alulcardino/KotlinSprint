ec74dc3b40700f547aa0e2ecfe356a8f39ade5dd
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-13 20:04:05 +0600

    KS-9-2 list add to recipe is added

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -1,5 +1,16 @@
 package lesson_9
 
 fun main() {
-
+    val recipe = arrayListOf("Egg", "Sugar", "Bread")
+    println(
+        "В рецепте есть базовые ингредиенты: $recipe." +
+                "\nЖелаете добавить еще?"
+    )
+    val answer = readLine().toString()
+    if (answer == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val ingredient = readLine().toString()
+        recipe.add(ingredient)
+        println("Теперь в рецепте есть следующие ингредиенты: $recipe")
+    }
 }
