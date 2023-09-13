76e0ac56635676da2d8e0989afe4d99d265630af
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-13 20:27:16 +0600

    KS-9-3 amount of portion is added

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -1,5 +1,13 @@
 package lesson_9
 
 fun main() {
+    val recipe = arrayListOf(2, 50, 15)
+    print("Enter amount of a portion: ")
+    val amountOfPortion = readLine()?.toInt() ?: 0
 
+    for (i in recipe.indices) {
+        recipe[i] *= amountOfPortion
+    }
+
+    println("На $amountOfPortion порций вам понадобится: Яиц – ${recipe[0]}, молока – ${recipe[1]}, сливочного масла – ${recipe[2]}]")
 }
