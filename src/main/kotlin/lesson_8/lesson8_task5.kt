9850ffec24137f4980d30dfaa5976eab12b2ee1c
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-13 19:29:40 +0600

    KS-8-5 enter recipe is added

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,5 +1,16 @@
 package lesson_8
 
+import com.sun.xml.internal.fastinfoset.util.StringArray
+
 fun main() {
+    print("Enter amount of Ingredients: ")
+    val amountOfIngredients =  readLine()?.toInt() ?: 0
+    val ingredients = Array(amountOfIngredients) {""}
+
+    for (i in ingredients.indices) {
+        println("Enter ingredient: ")
+        val ingredient = readLine().toString()
+        ingredients[i] = ingredient
+    }
 
 }
