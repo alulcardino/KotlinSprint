238ce1935c0e539ba37e6a5ccc23082a60c15e3a
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-15 01:33:05 +0600

    KS-9-4 recipe in order is added

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,5 +1,9 @@
 package lesson_9
 
 fun main() {
-
+    println("Enter 5 ingredients: ")
+    val ingredientsFromConsole = readln()
+    val ingredients = ingredientsFromConsole.split(",")
+    ingredients.sorted()
+    println(ingredients)
 }
