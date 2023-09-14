7d445557c6b1b36c3942d0fcd2d806dbce938714
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-15 01:01:30 +0600

    KS-8-5 readline replaced by reedln

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -4,12 +4,12 @@ import com.sun.xml.internal.fastinfoset.util.StringArray
 
 fun main() {
     print("Enter amount of Ingredients: ")
-    val amountOfIngredients =  readLine()?.toInt() ?: 0
-    val ingredients = Array(amountOfIngredients) {""}
+    val amountOfIngredients =  readln().toInt() ?: 0
+    val ingredients = Array(amountOfIngredients) { ""}
 
     for (i in ingredients.indices) {
         println("Enter ingredient: ")
-        val ingredient = readLine().toString()
+        val ingredient = readln()
         ingredients[i] = ingredient
     }
 
