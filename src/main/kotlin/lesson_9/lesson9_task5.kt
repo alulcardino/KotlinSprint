d17772e1ac5a57ebccc89fcd8ba492ddb49ed0b7
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-15 01:16:35 +0600

    KS-9-1 refactored printl

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,6 +2,7 @@ package lesson_9
 
 fun main() {
     val recipe = arrayListOf<String>("Egg", "Sugar", "Bread")
+    println("В рецепте есть следующие ингредиенты: ")
     for (ingredient in recipe) {
         println(ingredient)
     }
