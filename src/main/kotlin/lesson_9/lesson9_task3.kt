1d402e1f7c4226d0515876744a2ab64e7526e724
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-15 01:28:04 +0600

    KS-9-3 added map

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -1,13 +1,11 @@
 package lesson_9
 
 fun main() {
-    val recipe = arrayListOf(2, 50, 15)
+    var recipe = arrayListOf(2, 50, 15)
     print("Enter amount of a portion: ")
     val amountOfPortion = readLine()?.toInt() ?: 0
 
-    for (i in recipe.indices) {
-        recipe[i] *= amountOfPortion
-    }
+    recipe = recipe.map { it * amountOfPortion } as ArrayList
 
     println("На $amountOfPortion порций вам понадобится: Яиц – ${recipe[0]}, молока – ${recipe[1]}, сливочного масла – ${recipe[2]}]")
 }
