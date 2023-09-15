0385d5ad955d123b23987f51ef9c93405b7010dc
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-15 20:14:54 +0600

    KS-9-1 moved to right file

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -1,5 +1,9 @@
 package lesson_9
 
 fun main() {
-
+    val recipe = arrayListOf<String>("Egg", "Sugar", "Bread")
+    println("В рецепте есть следующие ингредиенты: ")
+    for (ingredient in recipe) {
+        println(ingredient)
+    }
 }
--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,9 +1,5 @@
 package lesson_9
 
 fun main() {
-    val recipe = arrayListOf<String>("Egg", "Sugar", "Bread")
-    println("В рецепте есть следующие ингредиенты: ")
-    for (ingredient in recipe) {
-        println(ingredient)
-    }
+
 }
