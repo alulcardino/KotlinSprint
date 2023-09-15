b05ad3188a5a5ccc508a1b8df3517d2511b0a659
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-16 02:23:44 +0600

    KS-9-5 set is added

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,14 +1,13 @@
 package lesson_9
 
 fun main() {
-    var list : MutableList<String> = mutableListOf()
+    var list : MutableCollection<String> = mutableSetOf()
     for (i in 0..4) {
         println("Enter ingredient: ")
         val ingredient = readln()
         list.add(ingredient)
     }
-    list = list.toSet().distinct().toMutableList()
-    list.sort()
+    list = list.sorted().toMutableList()
     list[0] = list[0].replaceFirstChar { it.uppercase() }
     list[4] = list[4].plus(".")
     list.map { print("$it ") }
