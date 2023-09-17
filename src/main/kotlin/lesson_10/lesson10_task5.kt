94c426450beb7ae22a46a19c5a58951033d12531
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-17 23:16:09 +0600

    KS-10-1 movet to right file

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -1,15 +1,5 @@
 package lesson_10
 
 fun main() {
-    val firstNumber = generateNumber()
-    val secondNumber = generateNumber()
-    if (firstNumber > secondNumber) {
-        println("Robots win")
-    } else if (firstNumber < secondNumber) {
-        println("Humanity wins")
-    } else {
-        println("Both wins")
-    }
-}
 
-fun generateNumber(): Int = (1..6).random()
+}
