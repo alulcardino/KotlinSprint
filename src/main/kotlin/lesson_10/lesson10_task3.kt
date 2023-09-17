a58c58eaafcdd8ea730952e23626e34339fe2d20
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-17 23:48:30 +0600

    KS-10-3 password generator is added

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,6 +1,27 @@
 package lesson_10
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    print("Enter length: ")
+    val length = readln().toInt()
+    val password = generatePassword(length)
+    println(password)
 }
 
+fun generatePassword(length: Int): String {
+    val numbers  = ('1'..'9')
+    val symbols = "!" + "\"" + "#" +
+            "\\" + "$" + "%" + "&" + "\'" + "(" + ")" +
+            "*" + "+" + "," + "-" + "." + "/" + " "
+    val password = StringBuilder("")
+    for (i in 1..length) {
+        if (i % 2 == 0) {
+            password.append(numbers.random())
+        } else {
+            password.append(symbols[Random.nextInt(symbols.length)])
+
+        }
+    }
+    return password.toString()
+}
