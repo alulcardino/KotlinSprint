a4f37e5a796ce069e4c2bbe821579d7ca37c7a81
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-19 19:59:26 +0600

    KS-11-3 list of avatars is added

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,5 +1,36 @@
 package lesson_11
 
 fun main() {
+    val statuses = listOf("разговаривает", "микрофон выключен", "пользователь заглушен")
+    val room1 =
+        Room(
+            "cover1", "name1",
+            listOf(Avatar(statuses[0]), Avatar(statuses[1]))
+        )
+    val room2 =
+        Room(
+            "cover2", "name2",
+            listOf(Avatar(statuses[0]), Avatar(statuses[1]), Avatar(statuses[2]), Avatar(statuses[1]))
+        )
+    println(room1)
+    println(room2)
+}
 
+
+private class Room(
+    private val cover: String,
+    private val name: String,
+    private val listOfMembers: List<Avatar>,
+) {
+    override fun toString(): String {
+        return "Room(cover='$cover', name='$name', listOfMembers=$listOfMembers)"
+    }
+}
+
+private class Avatar(
+    private val status: String
+) {
+    override fun toString(): String {
+        return "Avatar(status='$status')"
+    }
 }
