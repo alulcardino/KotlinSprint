00daef6b83180dd05ebf0a832ef1d4a28c40cec8
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-19 19:38:03 +0600

    KS-10-3 ascii is added

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -11,15 +11,13 @@ fun main() {
 
 fun generatePassword(length: Int): String {
     val numbers  = ('1'..'9')
-    val symbols = "!" + "\"" + "#" +
-            "\\" + "$" + "%" + "&" + "\'" + "(" + ")" +
-            "*" + "+" + "," + "-" + "." + "/" + " "
+    val symbols = (' '.code..'/'.code)
     val password = StringBuilder("")
     for (i in 1..length) {
         if (i % 2 == 0) {
             password.append(numbers.random())
         } else {
-            password.append(symbols[Random.nextInt(symbols.length)])
+            password.append(symbols.random())
 
         }
     }
