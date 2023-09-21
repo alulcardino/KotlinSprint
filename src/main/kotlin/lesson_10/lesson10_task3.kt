65005b3db71c7214baf51776d52ed423c5ea99db
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-21 22:31:07 +0600

    KS-11-4 moved to right file

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,25 +1,5 @@
 package lesson_10
 
 fun main() {
-    var firstNumber: Int
-    var secondNumber: Int
-    do {
-        firstNumber = generateNumbers()
-        secondNumber = generateNumbers()
-        if (firstNumber > secondNumber) {
-            println("Robots win")
-        } else if (firstNumber < secondNumber) {
-            println("Humanity wins")
-        } else {
-            println("Both wins")
-        }
-    } while (tryAgain())
-}
 
-private fun generateNumbers(): Int = (1..6).random()
-
-fun tryAgain(): Boolean {
-    println("Хотите бросить кости еще раз Введите Да или Нет”")
-    val answer = readln()
-    return answer.equals("да", ignoreCase = true)
 }
