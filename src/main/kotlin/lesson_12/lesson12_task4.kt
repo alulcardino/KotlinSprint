8557ef36b188f9ba35377332d9a62746b15934e8
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-22 18:32:03 +0600

    KS-12-4 weather init constructor is added

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -1,5 +1,24 @@
 package lesson_12
 
 fun main() {
+    val weather1 = Weather3(1, 2, false, 3)
+    val weather2 = Weather3(4, 6, true, 10)
+    val weather3 = Weather3(5, 7, false, 12)
+}
 
+class Weather3(
+    private var weatherNight: Int,
+    private var weatherDay: Int,
+    private var isRain: Boolean,
+    private var pressure: Int
+) {
+
+    init {
+        println(
+            "Weather(weatherNight=$weatherNight, weatherDay=$weatherDay," +
+                    " isRain=$isRain, pressure=$pressure)"
+        )
+
+
+    }
 }
