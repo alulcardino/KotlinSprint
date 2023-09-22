76541b319f1372e1aebabb4c0970a038efd9a5d0
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-22 18:24:40 +0600

    KS-12-3 updated weather constructor is added

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,5 +1,26 @@
 package lesson_12
 
 fun main() {
+    val weather1 = Weather1(1, 2, false, 3)
+    val weather2 = Weather1(4, 6, true, 10)
+    val weather3 = Weather1(5, 7, false, 12)
 
+    println(weather1)
+    println(weather2)
+    println(weather3)
+
+}
+
+class Weather1(
+    private var weatherNight: Int,
+    private var weatherDay: Int,
+    private var isRain: Boolean,
+    private var pressure: Int
+) {
+
+
+    override fun toString(): String {
+        return "Weather(weatherNight=$weatherNight, weatherDay=$weatherDay," +
+                " isRain=$isRain, pressure=$pressure)"
+    }
 }
