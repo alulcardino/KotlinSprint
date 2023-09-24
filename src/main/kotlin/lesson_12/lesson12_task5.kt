ea3c93a7188553fe08d29d86b058170e2d6d5fc7
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-24 14:34:45 +0600

    KS-12-5 average weather is added

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -1,5 +1,56 @@
 package lesson_12
 
 fun main() {
+    val pressureDiapason = (700..800)
+    val weatherDiapason = (10..30)
+    val list = mutableListOf<Weather4>()
+    for (i in 0..9) {
+        list.add(
+            Weather4(
+                weatherDiapason.random(),
+                weatherDiapason.random(),
+                i % 2 == 0,
+                pressureDiapason.random()
+            )
+        )
+    }
+    var averageTempDay  = 0.0
+    var averageTempNight = 0.0
+    var averageTemp = 0.0
+    var rainDay = 0
+    var averagePressure = 0.0
 
+    list.forEach {
+        averageTempDay += it.weatherDay
+        averageTempNight += it.weatherNight
+        if (it.isRain) {
+            rainDay++
+        }
+        averagePressure += it.pressure
+    }
+
+    averageTemp = (averageTempDay + averageTempNight) / 20
+    averagePressure /= 10
+    println("""
+        average temp = $averageTemp
+        average pressure = $averagePressure
+        rain days = $rainDay
+    """.trimIndent())
+}
+
+class Weather4(
+     var weatherNight: Int,
+     var weatherDay: Int,
+     var isRain: Boolean,
+     var pressure: Int
+) {
+
+    init {
+        println(
+            "Weather(weatherNight=$weatherNight, weatherDay=$weatherDay," +
+                    " isRain=$isRain, pressure=$pressure)"
+        )
+
+
+    }
 }
