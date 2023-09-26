fd14e976e1d0f4a344703080fe3326a1380480a9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-27 00:06:10 +0600

    KS-13-5 base class is init

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,5 +1,20 @@
 package lesson_14
 
 fun main() {
+    val ship1 = Ship("name1", 10, 100, 20)
+    val ship2 = Ship("name2", 20, 400, 600)
 
+    println(ship1)
+    println(ship2)
+}
+
+class Ship(
+    private val name: String,
+    private val speed: Int,
+    private val weight: Int,
+    private val boxes: Int,
+) {
+    override fun toString(): String {
+        return "Ship(name='$name', speed=$speed, weight=$weight, boxes=$boxes)"
+    }
 }
