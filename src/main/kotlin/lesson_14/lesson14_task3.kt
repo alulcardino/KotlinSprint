8c4fdd53a61a46fe9d4c98ea72189ab649b5cdfd
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-28 21:40:30 +0600

    KS-14-3 forum is added

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -1,5 +1,27 @@
 package lesson_14
 
 fun main() {
+    val comment = Comment("comment", "author1", Message("message", "author2"))
+    println(comment)
+}
 
+
+open class Message(
+    private val text : String,
+    private val author : String,
+) {
+    override fun toString(): String {
+        return "Message(text='$text', author='$author')"
+    }
+}
+
+class Comment(
+    private val text: String,
+    private val author: String,
+    private val message: Message,
+) : Message(text, author) {
+
+    override fun toString(): String {
+        return "Comment(text='$text', author='$author', message=$message)"
+    }
 }
