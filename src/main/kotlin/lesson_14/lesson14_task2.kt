eef03967a5f1d77122fbd3a80d8cfa27a0a7dc6b
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-09-28 11:18:09 +0600

    KS-14-2 added chil classes ship

--- src/main/kotlin/lesson_14/lesson14_task2.kt
+++ src/main/kotlin/lesson_14/lesson14_task2.kt
@@ -1,5 +1,44 @@
 package lesson_14
 
 fun main() {
+    val icebreaker = Icebreaker("name1", 10, 100, 20, 30)
+    val cargoShip = CargoShip("name2", 20, 400, 600, "EVERGREEN")
 
+    println(icebreaker)
+    icebreaker.breakIce()
+    println(cargoShip)
+}
+
+
+class Icebreaker(
+    name: String,
+    speed: Int,
+    weight: Int,
+    boxes: Int,
+    private val temp: Int,
+) : Ship1(name, speed, weight, boxes) {
+
+    fun breakIce() {
+        println("breaking ice...")
+    }
+}
+
+class CargoShip(
+    name: String,
+    speed: Int,
+    weight: Int,
+    boxes: Int,
+    private val company: String
+): Ship1(name, speed, weight, boxes)
+
+
+open class Ship1(
+    private val name: String,
+    private val speed: Int,
+    private val weight: Int,
+    private val boxes: Int,
+) {
+    override fun toString(): String {
+        return "Ship(name='$name', speed=$speed, weight=$weight, boxes=$boxes)"
+    }
 }
