3a3dfd22a82ab96399c78eee08ffea3890f0fa0d
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-09 20:48:09 +0600

    KS-16-1 moved to right file

--- src/main/kotlin/lesson_16/lesson16_task1.kt
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -1,5 +1,17 @@
 package lesson_16
 
 fun main() {
+    val dice = Dice()
+    dice.getNumber()
+}
 
+
+class Dice() {
+    private var number = generateNumber()
+
+    private fun generateNumber() = (1..6).random()
+
+    fun getNumber() {
+        println(number)
+    }
 }
--- src/main/kotlin/lesson_16/lesson16_task3.kt
+++ src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -1,17 +1,5 @@
 package lesson_16
 
 fun main() {
-    val dice = Dice()
-    dice.getNumber()
-}
 
-
-class Dice() {
-    private var number = generateNumber()
-
-    private fun generateNumber() = (1..6).random()
-
-    fun getNumber() {
-        println(number)
-    }
 }
