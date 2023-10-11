39c64a14dbc2912366095bf9ce87eaf50fd47164
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-11 21:59:59 +0600

    KS-16-3 user is added

--- src/main/kotlin/lesson_16/lesson16_task3.kt
+++ src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -1,5 +1,28 @@
 package lesson_16
 
 fun main() {
+    val user = User("login")
+    println("Enter login: ")
+    val password = "12345678"
+    user.setPassword(password)
+    if(user.validatePassword(password)) {
+        println("Password is correct")
+    } else {
+        println("Password isn't correct")
+    }
 
 }
+
+class User(
+    private val login: String
+) {
+    private var password: String = ""
+
+    fun setPassword(password: String) {
+        this.password = password
+    }
+
+    fun validatePassword(password: String) = password == this.password
+
+}
+
