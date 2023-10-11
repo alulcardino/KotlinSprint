cf1f6bf8d7ce6f33b105f06c782f56986821f406
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-11 21:31:42 +0600

    KS-16-2 circle is added

--- src/main/kotlin/lesson_16/lesson16_task1.kt
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -1,5 +1,25 @@
 package lesson_16
 
+import kotlin.math.pow
+
 fun main() {
+    val circle1 = Circle(10.0)
+    val circle2 = Circle(20.0)
+    println(circle1)
+    println(circle2)
+}
+
+class Circle(
+    private val radius: Double,
+) {
+    private val pi: Double = 3.14
+
+    fun getLength() = 2 * pi * radius
+
+    fun getSquare() = pi * radius.pow(2)
+    override fun toString(): String {
+        return "Circle(Length=${getLength()}, Square=${getSquare()})"
+    }
+
 
 }
