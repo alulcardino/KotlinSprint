28f11388caedac0900696e1fdf8510ccf012504c
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-11 21:21:02 +0600

    KS-16-1 reworked

--- src/main/kotlin/lesson_16/lesson16_task1.kt
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -3,15 +3,19 @@ package lesson_16
 fun main() {
     val dice = Dice()
     dice.getNumber()
+    dice.getNumber()
+    dice.getNumber()
+
 }
 
 
 class Dice() {
-    private var number = generateNumber()
+    private var number: Int = 0
 
     private fun generateNumber() = (1..6).random()
 
     fun getNumber() {
+        number = generateNumber()
         println(number)
     }
 }
