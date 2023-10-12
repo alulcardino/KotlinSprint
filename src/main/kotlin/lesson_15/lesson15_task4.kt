1920662e3d5d01a1fec12cb06680464bc515f0ff
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-12 21:39:43 +0600

    KS-15-5 moved to right file

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -1,55 +1,6 @@
 package lesson_15
 
 fun main() {
-    val truck = Truck(2, 1)
-    val car1 = PassengerCar(3)
-    val car2 = PassengerCar(2)
-    truck.drive()
-    truck.driveWithPassenger()
-    truck.driveWithCargo()
-    car1.drive()
-    car1.driveWithPassenger()
-    car2.drive()
-    car2.driveWithPassenger()
-}
-
-interface Drivable {
-    fun drive()
-}
-
-interface Passengerable {
-    fun driveWithPassenger()
 
 }
 
-interface Cargoble {
-    fun driveWithCargo()
-}
-
-abstract class Car(
-    private val amountOfPassenger: Int,
-) : Drivable, Passengerable {
-    override fun drive() {
-        println("driving....")
-    }
-
-    override fun driveWithPassenger() {
-        println("drive with $amountOfPassenger passengers")
-    }
-}
-
-class Truck(
-    private val amountOfCargo: Int, amountOfPassenger: Int,
-) : Car(amountOfPassenger), Cargoble {
-
-    override fun driveWithCargo() {
-        println("drive with $amountOfCargo cargos")
-    }
-
-}
-
-class PassengerCar(
-    amountOfPassenger: Int
-) : Car(amountOfPassenger) {
-
-}
