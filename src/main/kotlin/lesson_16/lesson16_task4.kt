e55b89faeb0b51d863867816e40d5669902e49ad
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-12 22:00:26 +0600

    KS-16-4 order is added

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -1,5 +1,35 @@
 package lesson_16
 
 fun main() {
+     val statuses = listOf("DELIVERED", "CANCELED", "IN PROGRESS")
+
+    val order1 = Order(11111)
+    val order2 = Order(12312512)
+    val order3 = Order(123125413)
+
+    order1.changeStatus(statuses[0])
+    order2.changeStatus(statuses[1])
+    order3.changeStatus(statuses[2])
+
+    println(order1)
+    println(order2)
+    println(order3)
 
 }
+
+class Order(
+    private val number: Long,
+) {
+    private var status: String = ""
+
+    fun changeStatus(status: String) {
+        this.status = status
+    }
+
+    override fun toString(): String {
+        return "Order(number=$number, status='$status')"
+    }
+
+
+}
+
