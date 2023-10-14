edb34e440e51c71dcbee81321498a4c8f4780c28
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-15 02:34:11 +0600

    KS-17-1 quiz is added

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -2,5 +2,30 @@ package lesson_17
 
 
 fun main() {
+    val quiz = Quiz("question", "answer")
+    println(quiz)
+    quiz.setAnswer("answer1")
+    quiz.setQuestion("question1")
+    println(quiz.getAnswer())
+    println(quiz)
+}
+
+class Quiz(
+    private var question: String,
+    private var answer: String,
+) {
+    fun setQuestion(question: String) {
+        this.question = question
+    }
+
+    fun setAnswer(answer: String) {
+        this.answer = answer
+    }
+
+    fun getAnswer() = answer
+
+    override fun toString(): String {
+        return "Quiz(question='$question', answer='$answer')"
+    }
 
 }
