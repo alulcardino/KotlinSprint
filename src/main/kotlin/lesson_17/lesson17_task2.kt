247299a1f2272674826397f205feebe9fe20605f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-18 14:48:10 +0600

    KS-17-2 ship is added

--- src/main/kotlin/lesson_17/lesson17_task2.kt
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -2,5 +2,35 @@ package lesson_17
 
 
 fun main() {
+    val ship = Ship()
+    ship.speed = 10
+    ship.port = "Port"
+    ship.name = "Name"
+    println(ship)
+}
+
+class Ship(
+
+) {
+    var name = ""
+        get() = field
+        set(value) {
+            println("Can not rename")
+        }
+    var speed = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+    var port = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    override fun toString(): String {
+        return "Ship(name='$name', speed=$speed, port='$port')"
+    }
+
 
 }
