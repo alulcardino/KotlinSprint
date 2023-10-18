276d8ab223f20057a7a8290068302f206660e85b
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-18 15:32:26 +0600

    KS-17-4 package is added

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -2,5 +2,29 @@ package lesson_17
 
 
 fun main() {
+    val package1 = Package()
+    package1.number = 123
+    package1.location = "location1"
+    package1.location = "location2"
+    package1.location = "location3"
+    println(package1.countOfMove)
+}
+
+class Package() {
+    var countOfMove = 0
+        get() = field
+    var number = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var location = ""
+        get() = field
+        set(value) {
+            countOfMove++
+            field = value
+        }
+
 
 }
