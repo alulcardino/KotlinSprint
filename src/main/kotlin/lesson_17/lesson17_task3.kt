589dc57533c1b308e124880c0415f682b975201d
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-18 15:03:01 +0600

    KS-17-3 files are added

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -2,5 +2,43 @@ package lesson_17
 
 
 fun main() {
+    val folder = Folder()
+    folder.name = "name"
+    folder.countOfFiles = 4
+    folder.isHide = false
+    println(folder)
+    folder.isHide = true
+    println(folder)
+
+
+}
+
+class Folder() {
+    var name = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var countOfFiles = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var isHide = false
+        get() = field
+        set(value) {
+            if (value) {
+                name = "Folder is hidden"
+                countOfFiles = 0
+            }
+            field = value
+        }
+
+    override fun toString(): String {
+        return "Folder(name='$name', countOfFiles=$countOfFiles, isHide=$isHide)"
+    }
+
 
 }
