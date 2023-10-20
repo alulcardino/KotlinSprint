9f778cfcb7649fe6ceb5874fa043f730fbbf8fb3
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-20 21:41:21 +0600

    KS-17-4 removed getters and setters

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -2,22 +2,19 @@ package lesson_17
 
 
 fun main() {
-    val package1 = Package()
-    package1.number = 123
+    val package1 = Package(123)
     package1.location = "location1"
     package1.location = "location2"
     package1.location = "location3"
     println(package1.countOfMove)
+    println(package1)
 }
 
-class Package() {
+class Package(
+    private val number: Int,
+) {
     var countOfMove = 0
         get() = field
-    var number = 0
-        get() = field
-        set(value) {
-            field = value
-        }
 
     var location = ""
         get() = field
@@ -25,6 +22,10 @@ class Package() {
             countOfMove++
             field = value
         }
+
+    override fun toString(): String {
+        return "Package(number=$number, countOfMove=$countOfMove, location='$location')"
+    }
 
 
 }
