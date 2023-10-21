40f36edfc7c1a33a69b69db5f590166a03d0b898
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-21 22:46:30 +0600

    KS-17-4 moved to right file

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -2,30 +2,5 @@ package lesson_17
 
 
 fun main() {
-    val package1 = Package(123, "location1")
-    package1.location = "location1"
-    package1.location = "location2"
-    package1.location = "location3"
-    println(package1.countOfMove)
-    println(package1)
-}
-
-class Package(
-    private val number: Int,
-    location: String,
-) {
-    var countOfMove = 0
-
-    var location: String = location
-        get() = field
-        set(value) {
-            countOfMove++
-            field = value
-        }
-
-    override fun toString(): String {
-        return "Package(number=$number, countOfMove=$countOfMove, location='$location')"
-    }
-
 
 }
