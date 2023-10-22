a9b38134d96d84273e4bfa8ebb8f62ef12368fd8
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-22 23:19:24 +0600

    KS-18-3 animals are added

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -1,5 +1,37 @@
 package lesson_18
 
 fun main() {
+    val fox = Fox()
+    val dog = Dog()
+    val cat = Cat()
+    fox.eat()
+    dog.eat()
+    cat.eat()
+}
 
+open class Animal(
+    private val name: String,
+    private val food: String,
+) {
+    open fun eat() {
+        println("$name ест $food")
+    }
+}
+
+class Fox() : Animal("Лиса", "ягоды") {
+    override fun eat() {
+        super.eat()
+    }
+}
+
+class Dog() : Animal("Собкак", "кости") {
+    override fun eat() {
+        super.eat()
+    }
+}
+
+class Cat() : Animal("Кошка", "рыбу") {
+    override fun eat() {
+        super.eat()
+    }
 }
