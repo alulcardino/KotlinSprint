2bbf639367ddb703b68ff166e455bfc13a5f9bee
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-23 23:34:38 +0600

    KS-18-3 refactored

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -7,31 +7,47 @@ fun main() {
     fox.eat()
     dog.eat()
     cat.eat()
+    fox.play()
+    dog.play()
+    cat.play()
+    fox.sleep()
+    dog.sleep()
+    cat.sleep()
 }
 
-open class Animal(
-    private val name: String,
-    private val food: String,
+abstract class Animal(
+    private val type: String
 ) {
-    open fun eat() {
-        println("$name ест $food")
+    abstract fun eat()
+
+    fun play() {
+        println("$type играет")
+    }
+
+    fun sleep() {
+        println("$type спит")
+    }
+
+}
+
+class Fox(
+) : Animal("Лиса") {
+    override fun eat() {
+        println("Лиса ест ягоды")
+    }
+
+}
+
+class Dog(
+) : Animal("Собака") {
+    override fun eat() {
+        println("Собака ест кости")
     }
 }
 
-class Fox() : Animal("Лиса", "ягоды") {
+class Cat(
+) : Animal("Кошка") {
     override fun eat() {
-        super.eat()
-    }
-}
-
-class Dog() : Animal("Собкак", "кости") {
-    override fun eat() {
-        super.eat()
-    }
-}
-
-class Cat() : Animal("Кошка", "рыбу") {
-    override fun eat() {
-        super.eat()
+        println("Кошка ест рыбу")
     }
 }
