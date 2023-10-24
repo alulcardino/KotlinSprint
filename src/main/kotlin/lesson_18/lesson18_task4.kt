7c3f1e608f454d3831f329de88d9f311be9bdbc9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-24 21:53:58 +0600

    KS-18-5 moved to right file

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -1,51 +1,5 @@
 package lesson_18
 
 fun main() {
-    val circle1 = Circle(1,2)
-    val circle2 = Circle(1.1,2.2)
-    val square1 = Square(1,2)
-    val square2 = Square(1.1,2.2)
-    val point1 = Point(1,2)
-    val point2 = Point(1.1,2.2)
 
-    circle1.draw()
-    circle2.draw()
-    square1.draw()
-    square2.draw()
-    point1.draw()
-    point2.draw()
-}
-
-abstract class Object<T>(
-    val x: T,
-    val y: T,
-) {
-    abstract fun draw()
-}
-
-open class Point<T>(
-    x: T,
-    y: T,
-) : Object<T>(x, y) {
-    override fun draw() {
-        println("Нарисовать точку в [$x, $y]")
-    }
-}
-
-open class Circle<T>(
-    x: T,
-    y: T,
-) : Object<T>(x, y) {
-    override fun draw() {
-        println("Нарисовать круг в [$x, $y]")
-    }
-}
-
-open class Square<T>(
-    x: T,
-    y: T,
-) : Object<T>(x, y) {
-    override fun draw() {
-        println("Нарисовать квадрат в [$x, $y]")
-    }
 }
