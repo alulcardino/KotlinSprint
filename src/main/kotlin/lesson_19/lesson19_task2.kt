61af44dee199b29ba49567d3498b6cdf9081bd7b
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-24 22:28:25 +0600

    KS-19-2 shop is added

--- src/main/kotlin/lesson_19/lesson19_task2.kt
+++ src/main/kotlin/lesson_19/lesson19_task2.kt
@@ -1,5 +1,31 @@
 package lesson_19
 
 fun main() {
+    val product = Product("product", "id", Category.OTHER)
+    println(product)
+}
 
+enum class Category() {
+    CLOTHES,
+    OFFICE,
+    OTHER,
+}
+
+fun setCategory(category: Category) : String{
+    return when(category) {
+        Category.CLOTHES -> "Одежда"
+        Category.OFFICE -> "Канцелярия"
+        Category.OTHER -> "Другое"
+    }
+}
+
+class Product(
+    private val name: String,
+    private val id: String,
+    private val category: Category,
+) {
+
+    override fun toString(): String {
+        return "Product(name='$name', id='$id', category=${setCategory(category)})"
+    }
 }
