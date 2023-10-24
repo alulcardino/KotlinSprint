09b43fea9416e4a09644645d660031d47433ad9f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-24 22:20:38 +0600

    KS-19-1 aquarium is added

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,5 +1,23 @@
 package lesson_19
 
 fun main() {
+    val fishes = listOf(Fish.GUPPY, Fish.GOLDFISH, Fish.ANGELFISH, Fish.SIAMESE_FIGHTING_FISH)
+    fishes.forEach{ setFish(it) }
+}
 
+
+enum class Fish(name: String) {
+    GUPPY("Гуппи"),
+    GOLDFISH("Золотая рыбка"),
+    ANGELFISH("Скалярия"),
+    SIAMESE_FIGHTING_FISH(("Петушок")),
+}
+
+fun setFish(fish: Fish) {
+    when (fish) {
+        Fish.GUPPY -> println("Гуппи")
+        Fish.GOLDFISH -> println("Золотая рыбка ")
+        Fish.ANGELFISH -> println("Скалярия")
+        Fish.SIAMESE_FIGHTING_FISH -> println("Петушок")
+    }
 }
