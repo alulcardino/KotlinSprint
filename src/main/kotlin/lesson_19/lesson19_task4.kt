99e453d3809179fd00515f05132e7b31a1be100f
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-25 12:36:02 +0600

    KS-19-4 tank is added

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -1,5 +1,25 @@
 package lesson_19
 
 fun main() {
+    val tank = Tank()
+    tank.bullet = Bullet.BLUE
+    tank.printDamage()
+    tank.bullet = Bullet.GREEN
+    tank.printDamage()
+    tank.bullet = Bullet.RED
+    tank.printDamage()
+}
 
+enum class Bullet(val damage: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20),
+}
+
+class Tank() {
+
+    var bullet: Bullet? = null
+    fun printDamage() {
+        println(bullet?.damage)
+    }
 }
