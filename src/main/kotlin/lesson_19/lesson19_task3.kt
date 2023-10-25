254663b383553296f131bd6b41aa46e8081e5de2
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-10-25 12:21:45 +0600

    KS-19-3 space ship is added

--- src/main/kotlin/lesson_19/lesson19_task3.kt
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -3,3 +3,21 @@ package lesson_19
 fun main() {
 
 }
+
+enum class Action{
+    FLY,
+    LAND,
+    SHOOT
+}
+
+class SpaceShip() {
+
+    fun todo() {
+        TODO()
+    }
+
+    fun exception() {
+        throw NotImplementedError()
+    }
+}
+
