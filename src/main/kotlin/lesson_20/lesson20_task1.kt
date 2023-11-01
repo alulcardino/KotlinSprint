f5d33ea033261de1d66c3cf1cc561f7a6464f451
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-01 19:33:02 +0600

    KS-20-1 splash screen is added

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -2,4 +2,12 @@ package lesson_20
 
 fun main() {
 
+    val greetings: (name: String) -> Unit
+
+    greetings = {
+        println("С наступающим Новым Годом, $it!")
+    }
+
+    greetings("name")
+
 }
