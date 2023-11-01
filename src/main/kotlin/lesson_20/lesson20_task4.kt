0e16b8d1ec989eb84b351aeab83e6c04b0e88c3b
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-01 20:44:35 +0600

    KS-20-4 elements is added

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,5 +1,22 @@
 package lesson_20
 
 fun main() {
+    val elements = listOf(
+        "element1",
+        "element2",
+        "element3",
+        "element4",
+        "element5"
+    )
+
+    val tap: (String) -> Unit = {
+        println("Нажат элемент $it")
+    }
+    elements.map {
+        if (elements.indexOf(it) % 2 == 1) {
+            tap(it)
+        }
+    }
+
 
 }
