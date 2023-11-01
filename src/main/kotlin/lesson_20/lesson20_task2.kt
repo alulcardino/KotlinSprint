5866e894b334db037d97e7d387de00415af693d9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-01 19:42:04 +0600

    KS-20-2 game is added

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -1,5 +1,24 @@
 package lesson_20
 
 fun main() {
+    val potion: (heal: Int,player: Player) -> Unit
+    potion = {
+        heal, player ->
+        player.currentHealth += heal
+    }
+    val player = Player("name", 5,10)
+    println(player)
+    potion(4, player)
+    println(player)
 
+}
+
+class Player(
+    private val name: String,
+    var currentHealth: Int,
+    private val maxHealth: Int,
+) {
+    override fun toString(): String {
+        return "Player(name='$name', currentHealth=$currentHealth, maxHealth=$maxHealth)"
+    }
 }
