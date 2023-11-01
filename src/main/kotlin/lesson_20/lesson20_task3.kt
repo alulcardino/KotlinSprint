2e70b418e25285c3e346bce586eaf4ffe073db97
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-01 20:30:27 +0600

    KS-20-3 door is added

--- src/main/kotlin/lesson_20/lesson20_task3.kt
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -1,5 +1,23 @@
 package lesson_20
 
 fun main() {
+    val player1 = Player1(true)
+    val openDoor : (Player1) -> Unit = {
+        if (it.haveKay){
+            println("Door is open")
+        } else {
+            println("Door is closed")
+        }
+    }
 
+    openDoor(player1)
+}
+
+
+class Player1(
+    val haveKay: Boolean
+) {
+    override fun toString(): String {
+        return "Player1(haveKay=$haveKay)"
+    }
 }
