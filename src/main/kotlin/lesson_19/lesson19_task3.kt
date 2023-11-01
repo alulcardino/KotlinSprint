1939036b9486fa5790d21a0f3866e9474267aa4c
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-01 19:09:08 +0600

    KS-19-5 validation is added

--- src/main/kotlin/lesson_19/lesson19_task3.kt
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -3,21 +3,3 @@ package lesson_19
 fun main() {
 
 }
-
-enum class Action{
-    FLY,
-    LAND,
-    SHOOT
-}
-
-class SpaceShip() {
-
-    fun todo() {
-        TODO()
-    }
-
-    fun exception() {
-        throw NotImplementedError()
-    }
-}
-
--- src/main/kotlin/lesson_19/lesson19_task5.kt
+++ src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -2,34 +2,36 @@ package lesson_19
 
 fun main() {
     println("Имя дожно быть длинее 3 символов. Пол должен быть Мужской или Женский")
-    val cardIndex = mutableListOf(
-        Human("name1", Gender.MALE),
-        Human("name2", Gender.FEMALE),
-        Human("name3", Gender.MALE),
-        Human("name4", Gender.MALE),
-        Human("nam", Gender.MALE),
-    )
+
+    val cardIndex = mutableListOf<Human>()
+    for (i in 0..2) {
+        print("Введите имя: ")
+        val name = readln()
+        print("Введите пол: ")
+        val gender = readln()
+        if (validateGender(gender) && validateName(name)) {
+            cardIndex.add(Human(name, Gender.valueOf(gender)))
+        } else {
+            println("Неверные данные")
+        }
+    }
     println(cardIndex)
 }
+
+private fun validateName(name: String) = name.length < 4
+
+private fun validateGender(gender: String) = gender == "мужской"
+        || gender == "женский"
 
 enum class Gender(val gender: String) {
     MALE("мужской"),
     FEMALE("женский"),
 }
 
-class Human() {
-    private var name: String = ""
+class Human(
+    private var name: String = "",
     private var gender: Gender? = null
-
-    constructor(
-        name: String,
-        gender: Gender,
-    ) : this() {
-        if (name.length > 4) {
-            this.name = name
-        }
-        this.gender = gender
-    }
+) {
 
     override fun toString(): String {
         return "Human(name='$name', gender=${gender?.gender})"
