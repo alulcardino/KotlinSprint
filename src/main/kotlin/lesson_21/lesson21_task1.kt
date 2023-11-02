ee5a222e285be1ccff5bcb2e44722c20496de5b9
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-02 12:36:38 +0600

    KS-21-5 extension string is added

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -1,5 +1,17 @@
 package lesson_21
 
 fun main() {
+    val word = "aeiofawqczd"
+    println(word.vowelCount(word))
+}
 
+fun String.vowelCount(word: String) : Int {
+    val vowel = listOf('a', 'u', 'i', 'e', 'y', 'o')
+    var countOfVowel = 0
+    word.forEach {
+        if(vowel.contains(it.lowercaseChar())) {
+            countOfVowel++
+        }
+    }
+    return countOfVowel
 }
