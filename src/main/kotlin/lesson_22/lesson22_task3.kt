7923261da3edc4d9c4c9bb4cb185fd9853ae8d88
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-03 19:33:26 +0600

    KS-22-3 data class is added

--- src/main/kotlin/lesson_22/lesson22_task3.kt
+++ src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -1,5 +1,16 @@
 package lesson_22
 
 fun main() {
+    val user = User7("name", "surname", 30)
+    val name = user.name
+    val surname = user.surname
+    val age = user.age
 
+    println("$name $surname $age")
 }
+
+data class User7(
+    val name: String,
+    val surname: String,
+    val age: Int,
+)
