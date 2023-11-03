3b5e45c1244534d96266a9031b0f60c292c780ff
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-11-03 18:19:50 +0600

    KS-21-4 file extension is added

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -1,5 +1,23 @@
 package lesson_21
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val file = File("dictionary.txt")
+    file.writeToWord(Word("hello", "привет", 4))
+}
+
+fun File.writeToWord(word: Word) {
+    this.writeText("")
+    this.appendText("${word.englishWord}|${word.russianWord}|${word.correctAnswersCount}\n")
+}
+
+data class Word(
+    val englishWord: String,
+    val russianWord: String,
+    var correctAnswersCount: Int,
+) {
+    override fun toString(): String {
+        return "Word(englishWord='$englishWord', russianWord='$russianWord', correctAnswersCount=$correctAnswersCount)"
+    }
 }
