bf7174204b5069809c5643d01f4142058b9d4569
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-12-20 01:20:08 +0600

    KS-22-5 guide to the galaxy is added

--- src/main/kotlin/lesson_22/lesson22_task5.kt
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -1,5 +1,26 @@
 package lesson_22
 
+import java.util.*
+
 fun main() {
+    val alphaCentauri = GalacticGuide(
+        "Alpha Centauri",
+        "Triple star system in the southern constellation of Centaurus",
+        placeDistance = 42465
+        )
+    println(alphaCentauri)
+}
+
+data class GalacticGuide(
+    val placeName: String = "",
+    val placeDesc: String = "",
+    val time: Date? = null,
+    val placeDistance: Int = 0,
+)
+
+private fun dateFormatter() {
 
 }
+
+
+
