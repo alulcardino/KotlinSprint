1ae09b6a57c2f3f08b54e7108df1f632db9d3071
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-12-20 01:41:29 +0600

    KS-15-2 weather control is added

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -1,5 +1,29 @@
 package lesson_15
 
 fun main() {
+    val temperature = Temperature(20)
+    val precipitationAmount = PrecipitationAmount(200)
+    val weatherServer = WeatherServer()
+    weatherServer.sendMessage(temperature)
+    weatherServer.sendMessage(precipitationAmount)
+}
 
+abstract class WeatherStationStats(
+    val amount: Int
+)
+
+class Temperature(amount: Int) : WeatherStationStats(amount) {}
+
+
+class PrecipitationAmount(amount: Int) : WeatherStationStats(amount) {}
+
+class WeatherServer(
+) {
+    fun sendMessage(weather: WeatherStationStats) {
+        if (weather is Temperature) {
+            println("Temperature is ${weather.amount}")
+        } else if (weather is PrecipitationAmount) {
+            println("Precipitatio nAmount is ${weather.amount}")
+        }
+    }
 }
